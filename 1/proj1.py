a905ccee8926c23e8e3f74fd10b670e3102f461c
Haonan Zhu <hz1396@nyu.edu>
2017-11-04 16:24:05 -0400

    figure 3 settings 2

--- 1/proj1.py
+++ 1/proj1.py
@@ -113,7 +113,7 @@ def mergeLines(diagLen, stLines):
         if i not in calculatedIdx:
             for j in range(i + 1, l):
                 if abs(stLines[i][0] - stLines[j][0]) / (diagLen * 2) < 0.05 and abs(
-                                stLines[i][1] - stLines[j][1]) / 90 < 0.05:
+                                stLines[i][1] - stLines[j][1]) < 5:
                     curCloseIdx.append(j)
             if len(curCloseIdx) > 1:
                 maxNumIdx = curCloseIdx[0]
@@ -132,7 +132,7 @@ def mergeLines(diagLen, stLines):
             mergedResult.append(stLines[i])
     return mergedResult
 
-def plotMergedLines(mergedLines, input):
+def plotSeparateLines(mergedLines, input):
     for line in mergedLines:
         rho = line[0]
         theta = np.deg2rad(line[1])
@@ -329,7 +329,8 @@ straightLines = filterLines(accumulator)
 mergedLines = mergeLines(diagLen, straightLines)
 print("straight lines:", len(straightLines))
 print("merged straight lines:", mergedLines)
-# plotMergedLines(mergedLines, img)
+# plotSeparateLines(straightLines, img)
+# plotSeparateLines(mergedLines, img)
 parallelLinesGroup = findParallelLinesPair(mergedLines)
 print("Parallel lines group:", parallelLinesGroup)
 validPairs = matchParallelLinesPairs(parallelLinesGroup)
